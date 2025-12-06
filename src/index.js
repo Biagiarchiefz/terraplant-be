b3ce4972539f11bdd644b266018e115fbde53ebb
Biagiarchiefz <biagiiarchie@gmail.com>
2025-12-06 14:51:55 +0700

    setup prisma & monggodb

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { prisma } from "./config/prismaConfig.js";
 
-
+dotenv.config();
 const app = express();
-const PORT = 8000;
+const port = process.env.PORT;
 
 
-    app.get("/", (req, res) => {
-        res.status(200).json({
-          message: "get data success",
-          data: {
-            title: "bunga sakuran",
-            harga: 20000
-          }
-        })
-    })
+app.use(express.json());
+app.use(cors());
 
 
-     app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "get data success",
+    data: {
+      title: "bunga sakuran",
+      harga: 20000,
+    },
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
