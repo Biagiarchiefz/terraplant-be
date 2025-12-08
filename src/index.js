4cd3f60b47b32f8baaaa642e0aa72e7c0937043f
RiyyaduSolihin <riyyadu1234@gmail.com>
2025-12-08 10:37:34 +0700

    fix: menganti port 8000 yang nabrak menjadi 8001

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import { authRoute } from "./routes/authRoute.js";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8001;
 
 
 app.use(express.json());
 app.use(cors());
 
+
+app.use("/api/auth", authRoute);
 
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -24,9 +26,12 @@ app.get("/", (req, res) => {
 });
 
 
+console.log("PORT:", port);
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
 
-app.use("/api/auth", authRoute);
+
+
