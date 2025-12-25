78663e3ab196d0626497b3cc00455e5c1a6a7363
rikirikandi <rikirikandi1242@gmail.com>
2025-12-25 18:56:51 +0700

    feat: add endpoint crud cart

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -17,3 +17,55 @@ export const add = async (req, res) => {
     });
   }
 };
+
+export const update = async (req, res) => {
+  const { id } = req.params;   // id cart
+  const { qty } = req.body;
+  // console.log(id)
+  // console.log(qty)
+
+  try {
+    const updateQty = await updateCart(id, qty);
+    res.status(200).json({
+      message: "update cart success",
+      data: updateQty,
+    });
+  } catch (err) {
+    res.status(400).json({
+      error: err.message,
+    });
+  }
+};
+
+export const getById = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const cart = await getCartById(userId);
+    res.status(200).json({
+      message: "get cart success",
+      data: cart,
+    });
+  } catch (err) {
+    res.status(400).json({
+      error: err.message,
+    });
+  }
+};
+
+
+export const deleteItem = async (req, res) => {
+  const { id } = req.params;  // cart id
+  const userId = req.user.id;  // dari jwt
+  // console.log(userId)
+
+  try {
+      await deleteItemCart(id, userId);
+      res.status(200).json({
+        message: "item berhasil dihapus dari cart"
+      })
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+
+}
