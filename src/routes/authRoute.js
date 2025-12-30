139a0fefbf03c19d5ac2f302ff946a8d0e9b061d
Biagiarchiefz <biagiiarchie@gmail.com>
2025-12-30 14:34:45 +0700

    feat: add login google

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,13 +1,19 @@
 import express from "express";
-import { login, me, register } from "../controllers/authController.js";
+import {
+  login,
+  me,
+  register,
+  googleLogin,
+} from "../controllers/authController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 
-
 router.post("/login", login);
+
+router.post("/google-login", googleLogin);
 
 // route ini di protect oleh middleware ( middleware ini sma seperti penjaga gerbang, dia akan mengecek setiap req client )
 router.get("/me", authMiddleware, me);
